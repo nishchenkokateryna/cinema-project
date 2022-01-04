2b38068535512c5f6cc888fc2f5af36f7763cfd2
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2022-01-04 15:57:30 +0200

    Added toString() to all models, removed empty lines

--- src/main/java/cinema/project/model/Ticket.java
+++ src/main/java/cinema/project/model/Ticket.java
@@ -47,9 +47,10 @@ public class Ticket {
 
     @Override
     public String toString() {
-        return "Ticket{" + "id=" + id
-              + ", movieSession=" + movieSession
-              + ", user=" + user + '}';
+        return "Ticket{"
+                + "id=" + id
+                + ", movieSession=" + movieSession
+                + ", user=" + user
+                + '}';
     }
-
 }
